82959f518c505dddfa8da96dadbd03a7b04c20c3
redvital <webmaster.redvital@gmail.com>
2023-01-11 00:24:08 -0400

    feat: add navlink

--- hookApp/src/useContext/Menu/NavBar.jsx
+++ hookApp/src/useContext/Menu/NavBar.jsx
@@ -1,13 +1,42 @@
-
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const NavBar = () => {
     return (
-        <div>
-            <Link to="/"> Home</Link>
-            <Link to="/about"> About</Link>
-            <Link to="/login"> Login</Link>
-        </div>
+        <>
+            <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+                <div className="container-fluid">
+                    <Link to="/" className="navbar-brand" href="#">
+                        UseContext
+                    </Link>
+
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <NavLink
+                                to="/"
+                                className={({isActive}) =>
+                                    `nav-link ${isActive ? "active" : ""}`
+                                }
+                            >
+                                Home
+                            </NavLink>
+                            <NavLink
+                                to="/about"
+                                className={({isActive}) =>
+                                    `nav-link ${isActive ? "active" : ""}`
+                                }
+                            >
+                                About
+                            </NavLink>
+                            {/* <li className="nav-item">
+                                <Link to="/login" className="nav-link" href="#">
+                                    Login
+                                </Link>
+                            </li> */}
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
     );
 };
 
