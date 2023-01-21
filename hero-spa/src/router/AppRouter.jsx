9d0adbc1e3466a65d4b3aabd7af9a91724f7c930
redvital <kervisvasquez24@gmail.com>
2023-01-21 16:30:40 -0400

    add navBar in route

--- hero-spa/src/router/AppRouter.jsx
+++ hero-spa/src/router/AppRouter.jsx
@@ -3,10 +3,12 @@ import App from "../App";
 import LoginPage from "../auth/pages/LoginPage";
 import DcPage from "../heroes/pages/DcPage";
 import MarvelPage from "../heroes/pages/MarvelPage";
+import { Navbar } from "../ui";
 
 const AppRouter = () => {
     return (
         <>
+            <Navbar />
             <Routes>
                 <Route path="/" element={<Navigate to="marvel" />} />
                 <Route path="marvel" element={<MarvelPage />} />
