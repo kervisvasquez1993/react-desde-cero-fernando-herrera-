e6d38daa3fb530fba72965afa17eadc419213024
redvital <kervisvasquez24@gmail.com>
2023-01-21 23:50:03 -0400

    feat: add data in pages of hero

--- hero-spa/src/heroes/pages/DcPage.jsx
+++ hero-spa/src/heroes/pages/DcPage.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import { getHeroByPublisher } from "../helpers/getheroBypublisher";
 
 export const DcPage = () => {
-  return (
-    <div>DcPage</div>
-  )
-}
+    const heroes = getHeroByPublisher("DC Comics");
+    return (
+        <>
+            <h1>Dc Page</h1>
+            {heroes.map((hero) => {
+                return <h3 key={hero.id}>{hero.superhero}</h3>;
+            })}
+        </>
+    );
+};
 
 // export default DcPage
--- hero-spa/src/heroes/pages/MarvelPage.jsx
+++ hero-spa/src/heroes/pages/MarvelPage.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import { getHeroByPublisher } from "../helpers/getheroBypublisher";
 
 export const MarvelPage = () => {
-  return (
-    <div>MarvelPage</div>
-  )
-}
+    const heroes = getHeroByPublisher("Marvel Comics");
+    return (
+        <>
+            <h2>Marvel Page</h2>
+            {heroes.map((hero) => (
+                <h4 key={hero.id}> {hero.superhero}</h4>
+            ))}
+        </>
+    );
+};
 
 // export default MarvelPage
