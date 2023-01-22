1c65da3fcff74d085655cf0c9f2d418eeb7e38d7
redvital <kervisvasquez24@gmail.com>
2023-01-21 22:36:11 -0400

    add usenavigate in loginPage

--- hero-spa/src/ui/components/NavBar.jsx
+++ hero-spa/src/ui/components/NavBar.jsx
@@ -1,6 +1,13 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
+    const navigate = useNavigate();
+    const onLogout = () => {
+        // console.log("Hola mundo");
+        navigate("/login", {
+            replace: true,
+        });
+    };
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
             <Link className="navbar-brand" to="/">
@@ -47,16 +54,13 @@ export const Navbar = () => {
 
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end ">
                 <ul className="navbar-nav ml-auto">
-                    {/* <NavLink
-                        className={({ isActive }) =>
-                            `nav-item nav-link ${isActive ? "active" : ""}`
-                        }
-                        to="/login"
-                    >
-                        Logout
-                    </NavLink> */}
                     <span className="nav-item nav-link text-info">kervis</span>
-                    <button className="nav-item nav-link btn   ">logout</button>
+                    <button
+                        className="nav-item nav-link btn "
+                        onClick={() => onLogout()}
+                    >
+                        logout
+                    </button>
                 </ul>
             </div>
         </nav>
