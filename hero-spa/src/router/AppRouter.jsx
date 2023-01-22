3b4165cf19055bd0a35c302ff1343cd06fa48002
redvital <kervisvasquez24@gmail.com>
2023-01-21 21:27:20 -0400

    feat: update routes in project

--- hero-spa/src/router/AppRouter.jsx
+++ hero-spa/src/router/AppRouter.jsx
@@ -1,18 +1,13 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import App from "../App";
-import {LoginPage} from "../auth/";
-import { DcPage, MarvelPage } from "../heroes/";
-import { Navbar } from "../ui";
+import { LoginPage } from "../auth/";
+import { HeroesRoutes } from "../heroes/";
 
 const AppRouter = () => {
     return (
         <>
-            <Navbar />
             <Routes>
-                <Route path="/" element={<Navigate to="marvel" />} />
-                <Route path="marvel" element={<MarvelPage />} />
-                <Route path="dc" element={<DcPage />} />
                 <Route path="login" element={<LoginPage />} />
+                <Route path="/*" element={<HeroesRoutes />} />
             </Routes>
         </>
     );
--- hero-spa/src/ui/components/NavBar.jsx
+++ hero-spa/src/ui/components/NavBar.jsx
@@ -26,6 +26,22 @@ export const Navbar = () => {
                     >
                         DC
                     </NavLink>
+                    <NavLink
+                        className={({ isActive }) =>
+                            `nav-item nav-link ${isActive ? "active" : ""}`
+                        }
+                        to="/hero"
+                    >
+                        Hero
+                    </NavLink>
+                    <NavLink
+                        className={({ isActive }) =>
+                            `nav-item nav-link ${isActive ? "active" : ""}`
+                        }
+                        to="/search"
+                    >
+                        Search
+                    </NavLink>
                 </div>
             </div>
 
