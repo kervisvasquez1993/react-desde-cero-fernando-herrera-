d8a2c35053f1e6360f4605a407c8236678a9175e
redvital <kervisvasquez24@gmail.com>
2023-01-30 17:33:55 -0400

    new project journan

--- hero-spa/src/router/AppRouter.jsx
+++ hero-spa/src/router/AppRouter.jsx
@@ -2,12 +2,22 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { LoginPage } from "../auth/";
 import { HeroesRoutes } from "../heroes/";
 import { PrivateRoute } from "./PrivateRoute";
+import { PublicRouter } from "./PublicRouter";
 
 const AppRouter = () => {
     return (
         <>
             <Routes>
-                <Route path="login" element={<LoginPage />} />
+                <Route
+                    path="login/*"
+                    element={
+                        <PublicRouter>
+                            <Route path="/*" element={<LoginPage />} />
+                        </PublicRouter>
+                    }
+                />
+
+                {/* <Route path="login" element={<LoginPage />} /> */}
                 <Route
                     path="/*"
                     element={
