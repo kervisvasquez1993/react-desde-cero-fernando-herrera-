413d70b53c53c6ba7d679493664dbb34c8998e69
redvital <kervisvasquez24@gmail.com>
2023-02-06 00:15:30 -0400

    feat: add style in page of login

--- journal-app/src/auth/pages/LoginPage.jsx
+++ journal-app/src/auth/pages/LoginPage.jsx
@@ -1,7 +1,55 @@
-import React from 'react'
+import { Grid, Typography, TextField, Button } from "@mui/material";
+import React from "react";
 
 export const LoginPage = () => {
-  return (
-    <div>LoginPage</div>
-  )
-}
+    return (
+        <Grid
+            container
+            spacing={0}
+            direction="column"
+            justifyContent="center"
+            sx={{
+                minHeight: "100vh",
+                backgroundColor: "primary.main",
+                padding: 4,
+            }}
+        >
+            <Grid
+                item
+                className="box-shadow"
+                xs={2}
+                sx={{ backgroundColor: "white", padding: 3, borderRadius: 2 }}
+            >
+                <Typography variant="h5" sx={{ mb: 1 }}>
+                    Login
+                </Typography>
+                <form action="">
+                    <Grid container>
+                        <Grid item xs={12} sx={{ p: 1 }}>
+                            <TextField
+                                id=""
+                                label="Correo"
+                                type="email"
+                                placeholder="correo@gmail.com"
+                                fullWidth
+                            />
+                        </Grid>
+                        <Grid item xs={12} sx={{ p: 1 }}>
+                            <TextField
+                                label="password"
+                                type="password"
+                                placeholder="password"
+                                fullWidth
+                            />
+                        </Grid>
+                    </Grid>
+                    <Grid container spacing={2} sx={{ p: 1 }}>
+                        <Grid item xs={12}>
+                            <Button variant="contained" fullWidth>Login</Button>
+                        </Grid>
+                    </Grid>
+                </form>
+            </Grid>
+        </Grid>
+    );
+};
