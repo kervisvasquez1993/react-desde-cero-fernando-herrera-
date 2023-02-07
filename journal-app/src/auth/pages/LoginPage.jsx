82388198c14dd185e3ac6d36c4cd09f621c32e51
redvital <kervisvasquez24@gmail.com>
2023-02-07 15:39:33 -0400

    feat: update component layout for page in login and register

--- journal-app/src/auth/pages/LoginPage.jsx
+++ journal-app/src/auth/pages/LoginPage.jsx
@@ -1,55 +1,54 @@
-import { Grid, Typography, TextField, Button } from "@mui/material";
-import React from "react";
+import { Link as RouteLink } from "react-router-dom";
+import { Google } from "@mui/icons-material";
+import { Grid, Typography, TextField, Button, Link } from "@mui/material";
+import { AuthLayout } from "../layouts/AuthLayout";
 
 export const LoginPage = () => {
     return (
-        <Grid
-            container
-            spacing={0}
-            direction="column"
-            justifyContent="center"
-            sx={{
-                minHeight: "100vh",
-                backgroundColor: "primary.main",
-                padding: 4,
-            }}
-        >
-            <Grid
-                item
-                className="box-shadow"
-                xs={2}
-                sx={{ backgroundColor: "white", padding: 3, borderRadius: 2 }}
-            >
-                <Typography variant="h5" sx={{ mb: 1 }}>
-                    Login
-                </Typography>
-                <form action="">
-                    <Grid container>
-                        <Grid item xs={12} sx={{ p: 1 }}>
-                            <TextField
-                                id=""
-                                label="Correo"
-                                type="email"
-                                placeholder="correo@gmail.com"
-                                fullWidth
-                            />
-                        </Grid>
-                        <Grid item xs={12} sx={{ p: 1 }}>
-                            <TextField
-                                label="password"
-                                type="password"
-                                placeholder="password"
-                                fullWidth
-                            />
-                        </Grid>
+        <AuthLayout title={"Login"}>
+            <form action="">
+                <Grid container>
+                    <Grid item xs={12} sx={{ p: 1 }}>
+                        <TextField
+                            id=""
+                            label="Correo"
+                            type="email"
+                            placeholder="correo@gmail.com"
+                            fullWidth
+                        />
                     </Grid>
-                    <Grid container spacing={2} sx={{ p: 1 }}>
-                        <Grid item xs={12}>
-                            <Button variant="contained" fullWidth>Login</Button>
-                        </Grid>
+                    <Grid item xs={12} sx={{ p: 1 }}>
+                        <TextField
+                            label="password"
+                            type="password"
+                            placeholder="password"
+                            fullWidth
+                        />
                     </Grid>
-                </form>
-            </Grid>
-        </Grid>
+                </Grid>
+                <Grid container spacing={2} sx={{ p: 1 }}>
+                    <Grid item xs={12} sm={6}>
+                        <Button variant="contained" fullWidth>
+                            Login
+                        </Button>
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <Button variant="contained" fullWidth>
+                            <Google />
+                            <Typography sx={{ ml: 1 }}> Goolge</Typography>
+                        </Button>
+                    </Grid>
+                    <Grid container direction="row" justifyContent="end">
+                        <Link
+                            component={RouteLink}
+                            color="inherit"
+                            to="/auth/register"
+                        >
+                            Crear una cuenta
+                        </Link>
+                    </Grid>
+                </Grid>
+            </form>
+        </AuthLayout>
     );
 };
--- journal-app/src/auth/pages/RegisterPage.jsx
+++ journal-app/src/auth/pages/RegisterPage.jsx
@@ -1,7 +1,58 @@
-import React from 'react'
+import { Button, Grid, Link, TextField, Typography } from "@mui/material";
+import React from "react";
+import { Link as RouteLink } from "react-router-dom";
+import { AuthLayout } from "../layouts/AuthLayout";
 
 export const RegisterPage = () => {
-  return (
-    <div>RegisterPage</div>
-  )
-}
+    return (
+        <AuthLayout title="register">
+            <form action="">
+                <Grid container>
+                <Grid item xs={12} sx={{ p: 1 }}>
+                        <TextField
+                            id=""
+                            label="Nombre"
+                            type="text"
+                            placeholder="Nombre Completo"
+                            fullWidth
+                        />
+                    </Grid>
+                    <Grid item xs={12} sx={{ p: 1 }}>
+                        <TextField
+                            id=""
+                            label="Correo"
+                            type="email"
+                            placeholder="correo@gmail.com"
+                            fullWidth
+                        />
+                    </Grid>
+                    <Grid item xs={12} sx={{ p: 1 }}>
+                        <TextField
+                            label="password"
+                            type="password"
+                            placeholder="password"
+                            fullWidth
+                        />
+                    </Grid>
+                </Grid>
+                <Grid container spacing={2} sx={{ p: 1 }}>
+                    <Grid item xs={12} sm={12}>
+                        <Button variant="contained" fullWidth>
+                            crear cuenta
+                        </Button>
+                    </Grid>
+                 
+                    <Grid container direction="row" justifyContent="end">
+                        <Link
+                            component={RouteLink}
+                            color="inherit"
+                            to="/auth/login"
+                        >
+                            Ir al Login
+                        </Link>
+                    </Grid>
+                </Grid>
+            </form>
+        </AuthLayout>
+    );
+};
