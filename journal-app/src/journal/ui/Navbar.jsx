1c0b885d83f437d40deb5aaaf85dcb3e514292d3
redvital <kervisvasquez24@gmail.com>
2023-02-09 15:31:46 -0400

    feat: navBar add in project

--- journal-app/src/journal/ui/Navbar.jsx
+++ journal-app/src/journal/ui/Navbar.jsx
@@ -1,20 +1,40 @@
-import { MenuOutlined } from "@mui/icons-material";
-import { AppBar, IconButton, Toolbar } from "@mui/material";
+import { LogoutOutlined, MenuOutlined } from "@mui/icons-material";
+import { AppBar, Grid, IconButton, Toolbar, Typography } from "@mui/material";
 import React from "react";
 
-export const Navbar = ({ draweWith = 240 }) => {
+export const Navbar = ({ draweWith = 2400 }) => {
     return (
         <AppBar
             position="fixed"
             sx={{
-                width: { sm: `calc(100% - ${draweWith})px` },
+                width: { sm: `calc(100% - ${draweWith}px)` },
                 ml: { sm: `${draweWith}px` },
             }}
         >
             <Toolbar>
-                <IconButton>
+                <IconButton
+                    color="inherit"
+                    edge="start"
+                    sx={{ mr: 2, display: { sm: "none" } }}
+                >
                     <MenuOutlined />
                 </IconButton>
+                <Grid container direction="row" justifyContent="space-between" alignItems="center">
+                    <Typography
+                        varinat="h6"
+                        noWrap
+                        component="div"
+                        color="initial"
+                    >
+                        {" "}
+                        JournalApp
+                    </Typography>
+                    <IconButton color="error" aria-label="" onClick={() => {
+                        console.log("hola")
+                    }}>
+                        <LogoutOutlined />
+                    </IconButton>
+                </Grid>
             </Toolbar>
         </AppBar>
     );
