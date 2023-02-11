4b8b2e54470aebc0419792b17bcbcf364d9ffa0d
redvital <kervisvasquez24@gmail.com>
2023-02-11 17:18:06 -0400

    feat: config of slice in pokemonapi

--- redux-toolkit/src/main.jsx
+++ redux-toolkit/src/main.jsx
@@ -4,11 +4,12 @@ import { store } from "./store/store";
 import { Provider } from "react-redux";
 import App from "./App";
 import "./index.css";
+import PokemonApp from "./PokemonApp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <Provider store={store}>
-            <App />
+            <PokemonApp />
         </Provider>
     </React.StrictMode>
 );
