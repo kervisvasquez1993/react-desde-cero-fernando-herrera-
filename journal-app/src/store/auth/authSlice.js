ac04a319f173db8ae3d757d4fd109c3a7031e55e
redvital <kervisvasquez24@gmail.com>
2023-02-12 15:25:32 -0400

    feat: add redux in proyecto de diario

---
+++ journal-app/src/store/auth/authSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const authSlice = createSlice({
+    name: "auth",
+    initialState: {
+        status: "checking", //not-authenticated , authenticated
+        uid: null,
+        email: null,
+        displayName: null,
+        photoURL: null,
+        erorrMessage: null,
+    },
+    reducers: {
+        login: (state, actions) => {},
+        logout: (state, payload) => {},
+        register: (state, actions) => {},
+        checkingCredentials: (state) => {},
+    },
+});
+// Action creators are generated for each case reducer function
+export const { login, logout, register, checkingCredentials } =
+    authSlice.actions;
