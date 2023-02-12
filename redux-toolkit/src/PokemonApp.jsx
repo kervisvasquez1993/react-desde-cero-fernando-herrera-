6e3231855b401ed6ff4b2288e6c4908b4e2a6d37
redvital <kervisvasquez24@gmail.com>
2023-02-11 22:17:18 -0400

    feat: first example in next

--- redux-toolkit/src/PokemonApp.jsx
+++ redux-toolkit/src/PokemonApp.jsx
@@ -1,13 +1,36 @@
 import React, { useEffect } from "react";
-import {  useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getPokemons } from "./store/slices/pokemon";
 const PokemonApp = () => {
     const dispach = useDispatch();
+    const { page, pokemons, isLoading } = useSelector((state) => state.pokemon);
+    // console.log(test)
+    console.log(page);
+    console.log(pokemons, isLoading);
+
     useEffect(() => {
         dispach(getPokemons());
-    }, []); 
+    }, []);
 
-    return <div>PokemonApp</div>;
+    return (
+        <>
+            <h1>Pokemon list</h1>
+            <hr />
+            <ul>
+                {isLoading ? (
+                    <div>Loading....</div>
+                ) : (
+                    pokemons.map((pokemon) => (
+                        <li key={pokemon.name}>
+                            <a href={pokemon.url}>{pokemon.name}</a>
+                        </li>
+                    ))
+                )}
+
+                <button onClick={() => dispach(getPokemons(page))}>next</button>
+            </ul>
+        </>
+    );
 };
 
 export default PokemonApp;
--- redux-toolkit/src/store/slices/pokemon/thunks.js
+++ redux-toolkit/src/store/slices/pokemon/thunks.js
@@ -5,17 +5,10 @@ export const getPokemons = (page = 0) => {
     return async (dispatch, getState) => {
         dispatch(startLoadingPokemon());
         // todo: realizar peticion http
-        // const result = await fetch(
-        //     `https://pokeapi.co/api/v2/pokemon/?limit=10&offset=${page}`
-        // );
-        // const data = await result.json();
-        // console.log(data.results);
-
         const response = await pokemonApi.get(
-            `pokemon/?limit=10&offset=${page}`
+            `pokemon/?limit=10&offset=${page * 10}`
         );
-        const data = response.data
-        // console.log(response.data);
+        const data = response.data;
         dispatch(setPokemons({ pokemons: data.results, page: page + 1 }));
     };
 };
